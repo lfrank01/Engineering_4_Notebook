b5fe446291eb1cccec4731513e0696129a3ca29c
Luke <57154006+lfrank01@users.noreply.github.com>
2021-09-30 10:15:02 -0400

    Added completed msp_challenge but not completely spicy

--- msp_challenge.py
+++ msp_challenge.py
@@ -13,23 +13,124 @@
 
 print('Hangman Game')
 
-def hangman_builder(option):
     
-    # Max number of turns before complete hang man = 7
+def hangman_string(number: int):
+    # Create hangman steps. \n used to condense lines. 
     
-    if option == 1: 
-        print("|----")
-        print("    |")
-        print("    0")
+    characters = [
+        "|----\n    |",                                     # 0
+        "|----\n    |\n    0",                              # 1
+        "|----\n    |\n    0\n    |\n",                     # 2
+        "|----\n    |\n    0\n   \|\n",                     # 3
+        "|----\n    |\n    0\n   \|/\n",                    # 4
+        "|----\n    |\n    0\n   \|/\n    |\n",             # 5
+        "|----\n    |\n    0\n   \|/\n    |\n   /\n",       # 6
+        "|----\n    |\n    0\n   \|/\n    |\n   / \ \n",    # 7
+        ]
+    return characters[number]
     
-def ask_guess():
-    pass
+    
+def check_letter(guessed_letter, word_before_guess, secret_word):
+    
+    answer = list(word_before_guess)  # Convert existing word to a list of characters
+    
+    # In a for loop, if there is a variable, comma, then a variable, the first variable will represent
+    # the index number and the second will represent the character that is that number in the string.
+    
+    for position, character in enumerate(secret_word):
+        if character == guessed_letter:  # we found a match
+            answer[position] = guessed_letter
+            
+    joined_answer = ''.join(answer)  
+    # This joins the list of characters into a string. The "" means that no spaces will go in 
+    # between each character in the string.
+    
+    return joined_answer
 
-secret_word = input("Player 1 choose your word: ")
+# Create function to make clearing the screen easier.
+def clear_screen(num_times: int):
+    print('\n' * num_times)
 
-hangman_builder(1) 
-
-print("Missed Letters: ")
-print("Secret Word = " + ("-" * len(secret_word)))
-print("Guess a letter: ")    
+# Create function to print out wrong guesses.
+def wrong_letter_bank(incorrect_letters):
+    word_bank = ', '.join(incorrect_letters)
     
+    return word_bank
+
+
+# Set play_again to "" to start the while loop.
+play_again = ""
+
+# Set word_bank to "" so that the incorrect word bank will print out nothing if a 
+# wrong letter has not been guessed.
+word_bank = ""
+
+
+while play_again != "x":
+
+    secret_word = input("Player 1 choose your word: ")
+
+    incorrect_letters = []
+    word_before_guess = "-" * len(secret_word)
+    wrong_guesses = 0
+    
+    clear_screen(50)
+    
+    print(hangman_string(wrong_guesses))
+    
+    # While loop breaks after 7 wrong guesses.
+    while wrong_guesses < 7:
+        
+        clear_screen(1)
+        
+        # Print guessed secret word
+        print('The word is: ' + word_before_guess)
+        
+        clear_screen(1)
+        
+        guessed_letter = input('Player 2: enter your guessed letter: ')
+        
+        clear_screen(50)
+        
+        word_after_guess = check_letter(
+            guessed_letter, 
+            word_before_guess, 
+            secret_word
+            )
+        
+        if word_after_guess == word_before_guess:
+            wrong_guesses = wrong_guesses + 1
+            incorrect_letters.append(guessed_letter)
+            print("That's not the letter. One step cloer to hanging.")
+            
+            word_bank = wrong_letter_bank(incorrect_letters)
+            
+            
+        else:
+            print("You got a letter correct!")
+            word_before_guess = word_after_guess
+            
+            if word_after_guess == secret_word:
+                clear_screen(50)
+                print("Nice job! You escaped the hangman.")
+                print("The word was: " + secret_word)
+                break
+        
+        clear_screen(1)
+        
+        print(hangman_string(wrong_guesses))
+        
+        clear_screen(1)
+        
+        print('Missed letters: ' + str(word_bank))
+        
+    if wrong_guesses >= 7:
+        print('\nGame Over. You ran out of guesses\n')
+    
+    play_again = input('Type (enter) to go again or (x) + (enter) to quit: ')
+    
+    if play_again == "":
+        wrong_guesses = 0
+        print('\n' * 50)
+    
+print('Thanks for playing')
