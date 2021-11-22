2b495f15669538d78e443929101f452442fbf9de
lfrank01 <lfrank01@charlottesvilleschools.org>
2021-11-22 15:40:02 +0000

    Added double blink

---
+++ double_led_blink.py
@@ -0,0 +1,17 @@
+import RPI.GPIO as GPIO
+from time import sleep
+
+GPIO.setmode(GPIO.BCM) # sets up numbering scheme as BCM 
+
+# Goal: when 1 led goes high, the other goes off. 
+# Print state of each LED
+# Blinking continues until user inputs "CTRL + c"
+
+# gpio -g mode 21 out
+# gpio -g mode 21 1
+
+# set up
+
+
+
+
