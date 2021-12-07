23c4c15254367193bd6af36a0e85a8cb5404ecc1
lfrank01 <lfrank01@charlottesvilleschools.org>
2021-12-07 15:36:53 +0000

    Added gpio start code

---
+++ gpio_i2c.py
@@ -0,0 +1,12 @@
+# Luke Frank, 12/7/21, gpio pins i2c
+
+import Adafruit_SSD1306
+import Adafruit_LSM_303
+import time
+
+# Use SDA and SCA? 9.81 m/s^2? Show accelerometer data on OLED
+# How to clear screen/refresh?
+# Import i2c maybe?
+
+print("Test")
+
