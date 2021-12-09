b400bc54712dbd611a0569e98dc88302a0e51091
Luke <57154006+lfrank01@users.noreply.github.com>
2021-12-09 09:44:17 -0500

    Coming back

--- gpio_i2c.py
+++ gpio_i2c.py
@@ -4,6 +4,11 @@ import Adafruit_SSD1306
 import Adafruit_LSM_303
 import time
 
+# Now for your assignment.  Write a new program by combining the two example scripts you have already used. 
+# Mash these two programs together so that the X, Y, and Z accelerations measured by your accelerometer are displayed on your little OLED display.  
+# And convert and scale the values to m/s2, rounded to two or three decimal places.  
+# So, when it's just sitting flat on the table, the z-acceleration should be roughly 9.81 m/s2.  Feel free to play with font faces and sizes.
+
 # Use SDA and SCA? 9.81 m/s^2? Show accelerometer data on OLED
 # How to clear screen/refresh?
 # Import i2c maybe?
