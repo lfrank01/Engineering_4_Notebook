4308f3e341e60edfcf0c41230f8d2792da984a72
Luke <57154006+lfrank01@users.noreply.github.com>
2021-12-09 10:25:49 -0500

    Trying to organize

--- combined_i2c_work.py
+++ combined_i2c_work.py
@@ -2,27 +2,23 @@
 
 # Accelerometer -----
 
-# Simple demo of of the LSM303 accelerometer & magnetometer library.
-# Will print the accelerometer & magnetometer X, Y, Z axis values every half
-# second.
-# Author: Tony DiCola
-# License: Public Domain
 import time
 
-# Import the LSM303 module.
+# Import the LSM303 module -- Accelerometer
 import Adafruit_LSM303
 
-# ------
+# Import the LSM303 module -- Accelerometer
+import Adafruit_LSM303
 
-# Screen
-
+# Import other Accelerometer libraries
 import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
+import Adafruit_SSD1306 -- Screen
 
 from PIL import Image
 from PIL import ImageDraw
 from PIL import ImageFont
 
+# Set up Raspberry Pi
 
 # Raspberry Pi pin configuration:
 RST = 24
@@ -32,20 +28,22 @@ SPI_PORT = 0
 SPI_DEVICE = 0
 
 
-# ------
- 
-  
-# Create Objects
 
-
-
-# Create a LSM303 instance.
+# Create a LSM303 (accelerometer) instance.
 lsm303 = Adafruit_LSM303.LSM303()
 
-# Set up display
+# Set up SSD1306 display 
 disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
 
+# Initialize process - not really sure what it means...
+
 disp.begin()
+font = ImageFont.load_default()
+
+
+
+
+
 # Clear display.
 disp.clear()
 disp.display()
@@ -75,29 +73,32 @@ disp.display()
 # Test run
 
 
-disp.clear()
-disp.display()
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
 
-# Read the X, Y, Z axis acceleration values and print them.
-accel, mag = lsm303.read()
 
-# Grab the X, Y, Z components from the reading and print them out.
-accel_x, accel_y, accel_z = accel
-mag_x, mag_y, mag_z = mag
+
+# disp.clear()
+# disp.display()
+# # Create blank image for drawing.
+# # Make sure to create image with mode '1' for 1-bit color.
+# width = disp.width
+# height = disp.height
+# image = Image.new('1', (width, height))
+
+# # Read the X, Y, Z axis acceleration values and print them.
+# accel, mag = lsm303.read()
+
+# # Grab the X, Y, Z components from the reading and print them out.
+# accel_x, accel_y, accel_z = accel
+# mag_x, mag_y, mag_z = mag
     
     
     
-    print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
-          accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
-    
-    
-    # Wait half a second and repeat.
-    time.sleep(0.5)
+# print('Accel X={0}, Accel Y={1}, Accel Z={2}, Mag X={3}, Mag Y={4}, Mag Z={5}'.format(
+# accel_x, accel_y, accel_z, mag_x, mag_y, mag_z))
+
+
+# # Wait half a second and repeat.
+# time.sleep(0.5)
 
     
   
@@ -108,92 +109,10 @@ mag_x, mag_y, mag_z = mag
   
   
   
-  
-  #shapes.py
-    
-import time
-
-import Adafruit_GPIO.SPI as SPI
-import Adafruit_SSD1306
-
-from PIL import Image
-from PIL import ImageDraw
-from PIL import ImageFont
-
-
-# Raspberry Pi pin configuration:
-RST = 24
-# Note the following are only used with SPI:
-DC = 23
-SPI_PORT = 0
-SPI_DEVICE = 0
 
 
 
-# 128x32 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST)
 
-# 128x64 display with hardware I2C:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST)
-
-# Note you can change the I2C address by passing an i2c_address parameter like:
-disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, i2c_address=0x3d)
-
-# Alternatively you can specify an explicit I2C bus number, for example
-# with the 128x32 display you would use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, i2c_bus=2)
-
-# 128x32 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# 128x64 display with hardware SPI:
-# disp = Adafruit_SSD1306.SSD1306_128_64(rst=RST, dc=DC, spi=SPI.SpiDev(SPI_PORT, SPI_DEVICE, max_speed_hz=8000000))
-
-# Alternatively you can specify a software SPI implementation by providing
-# digital GPIO pin numbers for all the required display pins.  For example
-# on a Raspberry Pi with the 128x32 display you might use:
-# disp = Adafruit_SSD1306.SSD1306_128_32(rst=RST, dc=DC, sclk=18, din=25, cs=22)
-
-# Initialize library.
-disp.begin()
-
-# Clear display.
-disp.clear()
-disp.display()
-
-# Create blank image for drawing.
-# Make sure to create image with mode '1' for 1-bit color.
-width = disp.width
-height = disp.height
-image = Image.new('1', (width, height))
-
-# Get drawing object to draw on image.
-draw = ImageDraw.Draw(image)
-
-# Draw a black filled box to clear the image.
-draw.rectangle((0,0,width,height), outline=0, fill=0)
-
-# Draw some shapes.
-# First define some constants to allow easy resizing of shapes.
-padding = 2
-shape_width = 20
-top = padding
-bottom = height-padding
-# Move left to right keeping track of the current x position for drawing shapes.
-x = padding
-# Draw an ellipse.
-draw.ellipse((x, top , x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a rectangle.
-draw.rectangle((x, top, x+shape_width, bottom), outline=255, fill=0)
-x += shape_width+padding
-# Draw a triangle.
-draw.polygon([(x, bottom), (x+shape_width/2, top), (x+shape_width, bottom)], outline=255, fill=0)
-x += shape_width+padding
-# Draw an X.
-draw.line((x, bottom, x+shape_width, top), fill=255)
-draw.line((x, top, x+shape_width, bottom), fill=255)
-x += shape_width+padding
 
 # Load default font.
 font = ImageFont.load_default()
